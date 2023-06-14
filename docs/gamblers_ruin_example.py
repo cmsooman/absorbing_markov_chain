911a63caab47bc7b3475a2ccf8040ab806e45f2f
cmsooman <sooman_craig@yahoo.co.uk>
2023-06-14 22:20:37 +0100

    Completed example usage script

--- docs/gamblers_ruin_example.py
+++ docs/gamblers_ruin_example.py
@@ -1,5 +1,5 @@
 import numpy as np
-from absorbing_markov_chain import AbsorbingMarkovChain
+from absorbing_markov_chain.absorbing_markov_chain import AbsorbingMarkovChain
 
 P = np.array(
     [
@@ -26,7 +26,7 @@ P_c = np.array(
 # Determine the number of bets the gambler makes before the game is over
 
 # First we create an object using te class. We know that there are 2 absorbing states and we can set the number of projected years to 1 for now
-gamblers_ruin_problem = AbsorbingMarkovChain(2, P_c, 1)
+gamblers_ruin_problem = AbsorbingMarkovChain(2, P_c)
 
 # First we calculate the fundamental matrix F
 F = gamblers_ruin_problem.fundamental_matrix()
@@ -41,3 +41,23 @@ solution_matrix = gamblers_ruin_problem.absorb_probs()
 print(solution_matrix[2])
 
 # So the probabilty of the gambler starting with £30 facing financial ruin is 64% and the probability of reaching £50 is 36%.
+
+# As a sense check, find the power n at which the matrix remains stable
+long_run_matrix = gamblers_ruin_problem.forward_matrix(1)
+print(long_run_matrix)
+
+count = 1
+current_matrix = gamblers_ruin_problem.forward_matrix(count)
+updated_matrix = gamblers_ruin_problem.forward_matrix(count +1)
+while not np.allclose(current_matrix, updated_matrix, rtol=1e-05, atol=1e-08, equal_nan=False):
+    current_matrix = gamblers_ruin_problem.forward_matrix(count)
+    updated_matrix = gamblers_ruin_problem.forward_matrix(count +1)
+    count +=1
+print(count)
+print(updated_matrix[0:4, 4:updated_matrix.shape[1]])
+
+print(solution_matrix)
+
+avg_absorb_times = gamblers_ruin_problem.absorb_times()
+print(avg_absorb_times)
+
